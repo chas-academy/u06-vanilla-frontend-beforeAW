25645925e557657d8d553072b16bfad2bca3c1fb
Albin Wrebo <albin.wrebo@outlook.com>
2025-04-16 07:08:08 +0200

    init project

---
+++ src/index.ts
@@ -0,0 +1,5 @@
+// This file is the entry point of the application. It initializes the application and imports the main CSS file.
+
+import './styles/index.css';
+
+console.log('Application is running');
